2a99cf34b5829f1c99e62a914aaf0f7dd6cc631d
Dem Pilafian <dem@centerkey.com>
2018-11-16 18:16:12 -0800

    Separate color for boolean in json pretty print
    http://blog.centerkey.com/2013/05/javascript-colorized-pretty-print-json.html

--- js/library.js
+++ js/library.js
@@ -298,15 +298,19 @@ library.form = {
 
 library.json = {
    prettyPrint: function(obj) {
+      // Pretty Print JSON Data in Color
       function replacer(match, pIndent, pKey, pVal, pEnd) {
-         const key = '<span class=json-key>';
-         const val = '<span class=json-value>';
-         const str = '<span class=json-string>';
+         const key =  '<span class=json-key>';
+         const val =  '<span class=json-value>';
+         const bool = '<span class=json-boolean>';
+         const str =  '<span class=json-string>';
+         const isBool = ['true', 'false'].includes(pVal);
+         const pValSpan = /^"/.test(pVal) ? str : isBool ? bool : val;
          let r = pIndent || '';
          if (pKey)
             r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
          if (pVal)
-            r = r + (pVal[0] === '"' ? str : val) + pVal + '</span>';
+            r = r + pValSpan + pVal + '</span>';
          return r + (pEnd || '');
          }
       const jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
