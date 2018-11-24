909c0f3a16c1b7a79189f75ba816fd00c14149e2
Dem Pilafian <dem@centerkey.com>
2018-11-24 02:04:10 -0800

    Superseded by require('pretty-print-json')
    https://github.com/center-key/pretty-print-json

--- js/library.js
+++ js/library.js
@@ -296,41 +296,6 @@ library.form = {
       }
    };
 
-library.json = {
-   prettyPrint: function(obj) {
-      // Pretty Print JSON Data in Color
-      function replacer(match, pIndent, pKey, pVal, pEnd) {
-         const key =  '<span class=json-key>';
-         const val =  '<span class=json-value>';
-         const bool = '<span class=json-boolean>';
-         const str =  '<span class=json-string>';
-         const isBool = ['true', 'false'].includes(pVal);
-         const pValSpan = /^"/.test(pVal) ? str : isBool ? bool : val;
-         let r = pIndent || '';
-         if (pKey)
-            r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-         if (pVal)
-            r = r + pValSpan + pVal + '</span>';
-         return r + (pEnd || '');
-         }
-      const jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-      return JSON.stringify(obj, null, 3)
-         .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-         .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-         .replace(jsonLine, replacer);
-      },
-   printable: function(obj) {
-      // Converts an object into a printable string (with spaces).
-      // Example:
-      //    console.assert(library.json.printable({a:3,b:'bat'}) === "{ a: 3, b: 'bat' }");
-      return JSON.stringify(obj)
-         .replace(/"([^"]*)":/g, ' $1: ')
-         .replace(/}/g, ' }')
-         .replace(/,([^ ])/g, ', $1')
-         .replace(/"/g, "'");
-      }
-   };
-
 // Social bookmarking
 library.social = {
    // Usage:
