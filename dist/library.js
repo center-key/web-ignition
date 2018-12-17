7a45f80203fdb36de7ed112af16643e02dab77fb
Dem Pilafian <dem@centerkey.com>
2018-12-17 00:58:32 -0800

    Release v1.0.6

--- dist/library.js
+++ dist/library.js
@@ -1,7 +1,7 @@
-//! library.js ~ web-ignition v1.0.5 ~ github.com/center-key/web-ignition ~ MIT License
+//! library.js ~ web-ignition v1.0.6 ~ github.com/center-key/web-ignition ~ MIT License
 
 const library = {
-   version: '1.0.5',
+   version: '1.0.6',
    initialize: () => {
       $.fn.id =      library.ui.id;
       $.fn.enable =  library.ui.enable;
