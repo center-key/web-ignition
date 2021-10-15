a6beebe5ba2b715589e5fc56902c68fe4b2d3c8f
dpilafian <dem@centerkey.com>
2021-10-14 20:07:54 -0700

    Move task management from gulpfile to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,21 @@
 // Gulp tasks
 
 // Imports
-import babel from           'gulp-babel';
-import css from             'gulp-postcss';
+import babel           from 'gulp-babel';
+import css             from 'gulp-postcss';
 import cssFontMagician from 'postcss-font-magician';
-import cssNano from         'cssnano';
-import cssPresetEnv from    'postcss-preset-env';
-import gap from             'gulp-append-prepend';
-import gulp from            'gulp';
-import less from            'gulp-less';
-import mergeStream from     'merge-stream';
-import rename from          'gulp-rename';
-import replace from         'gulp-replace';
-import size from            'gulp-size';
+import cssNano         from 'cssnano';
+import cssPresetEnv    from 'postcss-preset-env';
+import gap             from 'gulp-append-prepend';
+import gulp            from 'gulp';
+import less            from 'gulp-less';
+import rename          from 'gulp-rename';
+import replace         from 'gulp-replace';
+import size            from 'gulp-size';
 
 // Setup
-const transpileES6 =   ['@babel/env', { modules: false }];
-const babelMinifyJs =  { presets: [transpileES6, 'minify'], comments: false };
+const transpileES6 =  ['@babel/env', { modules: false }];
+const babelMinifyJs = { presets: [transpileES6, 'minify'], comments: false };
 const cssPlugins = [
    cssFontMagician({ protocol: 'https:' }),
    cssPresetEnv(),
@@ -27,55 +26,57 @@ const cssPlugins = [
 // Tasks
 const task = {
 
-   makeDistribution() {
-      const buildReset = () =>
-         gulp.src('css/reset.less')
-            .pipe(less())
-            .pipe(css(cssPlugins))
-            .pipe(rename({ extname: '.min.css' }))
-            .pipe(gap.appendFile('css/reset-color-overrides.css'))
-            .pipe(gap.appendText('\n'))
-            .pipe(size({ showFiles: true }))
-            .pipe(gulp.dest('build'));
-      const buildBloggerTweaks = () =>
-         gulp.src('css/blogger-tweaks/style.less')
-            .pipe(less())
-            .pipe(css(cssPlugins))
-            .pipe(rename('blogger-tweaks.min.css'))
-            .pipe(gap.appendText('\n'))
-            .pipe(gap.appendFile('css/blogger-tweaks/instructions.css'))
-            .pipe(gap.appendText('\n'))
-            .pipe(size({ showFiles: true }))
-            .pipe(gulp.dest('build'));
-      const buildLayoutsJs = () =>
-         gulp.src('css/layouts/*.js')
-            .pipe(babel(babelMinifyJs))
-            .pipe(rename({ extname: '.min.js' }))
-            .pipe(gap.appendText('\n'))
-            .pipe(size({ showFiles: true }))
-            .pipe(gulp.dest('build/layouts'));
-      const buildJs = () =>
-         gulp.src('build/lib-x.js')
-            .pipe(replace(/^import.*\n/m, ''))
-            .pipe(replace(/^export.*\n/m, ''))
-            .pipe(rename({ extname: '.dev.js' }))
-            .pipe(size({ showFiles: true }))
-            .pipe(gulp.dest('build'))
-            .pipe(babel(babelMinifyJs))
-            .pipe(rename('lib-x.min.js'))
-            .pipe(gap.appendText('\n'))
-            .pipe(size({ showFiles: true }))
-            .pipe(size({ showFiles: true, gzip: true }))
-            .pipe(gulp.dest('build'));
-      return mergeStream(
-         buildReset(),
-         buildBloggerTweaks(),
-         buildLayoutsJs(),
-         buildJs()
-         );
+   buildResetCss() {
+      return gulp.src('css/reset.less')
+         .pipe(less())
+         .pipe(size({ showFiles: true, gzip: true }))  //workaround to prevent "Error: read ECONNRESET" (revisit with Gulp v5.0)
+         .pipe(css(cssPlugins))
+         .pipe(rename({ extname: '.min.css' }))
+         .pipe(gap.appendFile('css/reset-color-overrides.css'))
+         .pipe(gap.appendText('\n'))
+         .pipe(size({ showFiles: true }))
+         .pipe(gulp.dest('build'));
+      },
+
+   buildBloggerCss() {
+      return gulp.src('css/blogger-tweaks/style.less')
+         .pipe(less())
+         .pipe(css(cssPlugins))
+         .pipe(rename('blogger-tweaks.min.css'))
+         .pipe(gap.appendText('\n'))
+         .pipe(gap.appendFile('css/blogger-tweaks/instructions.css'))
+         .pipe(gap.appendText('\n'))
+         .pipe(size({ showFiles: true }))
+         .pipe(gulp.dest('build'));
+      },
+
+   buildLayoutsJs() {
+      return gulp.src('css/layouts/*.js')
+         .pipe(babel(babelMinifyJs))
+         .pipe(rename({ extname: '.min.js' }))
+         .pipe(gap.appendText('\n'))
+         .pipe(size({ showFiles: true }))
+         .pipe(gulp.dest('build/layouts'));
+      },
+
+   buildLibJs() {
+      return gulp.src('build/lib-x.js')
+         .pipe(replace(/^import.*\n/m, ''))
+         .pipe(replace(/^export.*\n/m, ''))
+         .pipe(rename({ extname: '.dev.js' }))
+         .pipe(size({ showFiles: true }))
+         .pipe(gulp.dest('build'))
+         .pipe(babel(babelMinifyJs))
+         .pipe(rename('lib-x.min.js'))
+         .pipe(gap.appendText('\n'))
+         .pipe(size({ showFiles: true }))
+         .pipe(gulp.dest('build'));
       },
 
    };
 
 // Gulp
-gulp.task('build-files', task.makeDistribution);
+gulp.task('build-reset-css',   task.buildResetCss);
+gulp.task('build-blogger-css', task.buildBloggerCss);
+gulp.task('build-layouts-js',  task.buildLayoutsJs);
+gulp.task('build-lib-js',      task.buildLibJs);
