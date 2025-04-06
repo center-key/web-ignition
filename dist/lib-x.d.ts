1cd51fe479208da12dd0159047571c8568736db9
dpilafian <dem@centerkey.com>
2025-04-06 16:30:31 -0700

    Release v2.3.4

--- dist/lib-x.d.ts
+++ dist/lib-x.d.ts
@@ -1,4 +1,4 @@
-//! web-ignition v2.3.3 ~~ https://github.com/center-key/web-ignition ~~ MIT License
+//! web-ignition v2.3.4 ~~ https://github.com/center-key/web-ignition ~~ MIT License
 
 export type GlobalKey = keyof typeof globalThis;
 export type Json = string | number | boolean | null | undefined | JsonObject | Json[];
