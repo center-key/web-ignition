71b65cb0158be2f35951c5077a0fd72d7de9919f
dpilafian <dem@centerkey.com>
2025-05-03 02:57:22 -0700

    Explicitly set colors to prevent dark mode conflicts

--- src/css/layouts/neon.js
+++ src/css/layouts/neon.js
@@ -1,4 +1,4 @@
-// Neon layout -- For hackers and space junkies
+// Neon Layout -- For hackers and space junkies
 const numNeonImages = 8;
 const randomImage =   Date.now() % numNeonImages + 1;
 globalThis.document.body.classList.add('neon-image-' + String(randomImage));
