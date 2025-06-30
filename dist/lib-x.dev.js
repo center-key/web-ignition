2184c30f75e9ef3d9dff090c0973fb180d05604c
dpilafian <dem@centerkey.com>
2025-06-30 01:05:23 -0700

    Release v2.4.0

--- dist/lib-x.dev.js
+++ dist/lib-x.dev.js
@@ -1,4 +1,4 @@
-//! web-ignition v2.3.9 ~~ https://github.com/center-key/web-ignition ~~ MIT License
+//! web-ignition v2.4.0 ~~ https://github.com/center-key/web-ignition ~~ MIT License
 
 const libXDom = {
     stateDepot: [],
@@ -904,7 +904,7 @@ const libXExtra = {
     },
 };
 const libX = {
-    version: '2.3.9',
+    version: '2.4.0',
     dom: libXDom,
     ui: libXUi,
     util: libXUtil,
